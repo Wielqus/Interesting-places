cc86de1d0b2b5e776b0f9c59d7a82eab60722f00
Kuba <Wielqus@gmail.com>
2019-03-18 19:16:11 +0100

    Podpięcie FireBase

--- app/src/main/java/com/example/interestingplaces/MapsActivity.java
+++ app/src/main/java/com/example/interestingplaces/MapsActivity.java
@@ -1,13 +1,11 @@
 package com.example.interestingplaces;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
