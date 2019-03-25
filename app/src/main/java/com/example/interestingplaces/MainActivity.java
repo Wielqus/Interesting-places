a287a028360df8cd194f004f26a99b3247f49056
Kuba <Wielqus@gmail.com>
2019-03-25 18:56:18 +0100

    Zrobienie systemu logowania i rejestracji

---
+++ app/src/main/java/com/example/interestingplaces/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.interestingplaces;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button signOutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signOutButton = findViewById(R.id.logOutButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+    }
+
+    /**
+     * Sing out user
+     */
+    public void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
+                        MainActivity.this.startActivity(myIntent);
+                    }
+                });
+    }
+
+}
