9508a9f2cf06d8ad24dd480fb2c97eafe5ab6326
Wielqus <Wielqus@gmail.com>
2019-06-16 13:41:58 +0200

    Feature add place

--- app/src/main/java/com/example/interestingplaces/AddPlaceActivity.java
+++ app/src/main/java/com/example/interestingplaces/AddPlaceActivity.java
@@ -9,13 +9,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.gson.Gson;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,14 +28,30 @@ import java.util.Map;
 public class AddPlaceActivity extends AppCompatActivity {
 
     MaterialButton addButton;
+    MaterialButton selectPlaceButton;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private EditText name;
+    private EditText description;
+    private LatLng position;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place);
+        name = findViewById(R.id.placeName);
+        description = findViewById(R.id.placeDescription);
+        selectPlaceButton = findViewById(R.id.selectPlaceButton);
+        position = new LatLng(20.1123,20.323);
 
+        selectPlaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddPlaceActivity.this, SelectPlaceActivity.class);
+                startActivityForResult(intent, 200);
+
+            }
+        });
 
 
         addButton = findViewById(R.id.addButton);
@@ -39,14 +59,16 @@ public class AddPlaceActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Map<String, Object> place = new HashMap<>();
-                place.put("name", "Miejsce");
-                place.put("description", "Opis");
+                place.put("name", name.getText().toString());
+                place.put("description", description.getText().toString());
+                place.put("position", new GeoPoint(position.latitude,position.longitude));
                 db.collection("Places")
                         .add(place)
                         .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                             @Override
                             public void onSuccess(DocumentReference documentReference) {
-                                Log.d("FIRESTORE", "DocumentSnapshot added with ID: " + documentReference.getId());
+                                Intent myIntent = new Intent(AddPlaceActivity.this, MainActivity.class);
+                                AddPlaceActivity.this.startActivity(myIntent);
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
@@ -57,6 +79,21 @@ public class AddPlaceActivity extends AppCompatActivity {
                         });
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+
+            Bundle bundle = data.getExtras();
+            String positionString = bundle.getString("position");
+
+            position = new Gson().fromJson(positionString, LatLng.class);
+
+
+        } else if(resultCode == RESULT_CANCELED) {
+            Log.d("FingerPainter", "MainActivity canceled");
+
+        }
     }
 }
--- app/src/main/java/com/example/interestingplaces/SelectPlaceActivity.java
+++ app/src/main/java/com/example/interestingplaces/SelectPlaceActivity.java
@@ -1,25 +1,91 @@
 package com.example.interestingplaces;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
 
-public class SelectPlaceActivity extends AppCompatActivity {
+public class SelectPlaceActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLng position;
+    private Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_place);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        okButton = findViewById(R.id.okButton);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("position", new Gson().toJson(position));
+
+                Intent intent = new Intent();
+                intent.putExtras(bundle);
+                setResult(Activity.RESULT_OK, intent);
+
+                finish();
+            }
+        });
+
+
     }
 
     public void onMapReady(GoogleMap map) {
         mMap = map;
 
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                position = latLng;
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                // Clears the previously touched position
+                mMap.clear();
+
+                // Animating to the touched position
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                mMap.addMarker(markerOptions);
+            }
+        });
+
     }
+
+
 
 }
